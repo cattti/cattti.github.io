c2e273957d3efe6be71365d537b0e23d3d1b7743
Zavoianu Catinca <catinca.zavoianu@gmail.com>
2023-05-24 16:42:02 +0300

    added pages

---
+++ proiect.js
@@ -0,0 +1,9 @@
+
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Code to modify the footer color
+//     var body = document.body;
+//   if (body) {
+//     body.style.backgroundColor = "white";
+//   }
+// });
